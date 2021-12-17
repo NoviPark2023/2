9bd59363c42eae037ddcfd551d35ad4d3289dffc
natasa.pesic <natasa.pesic@factoryww.com>
2021-12-17 11:58:01 +0100

    layout fixes on reports

---
+++ src/Pages/Izvestaji/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PregledIzvestaja from 'Tabele/PregledIzvestaja';
+
+function Izvestaj() {
+  return (
+    <div>
+      <PregledIzvestaja />
+    </div>
+  );
+}
+
+export default Izvestaj;
