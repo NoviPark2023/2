7205beb3b5a99037ca68926b9470810ce60823c2
slobodantomic1989 <slobodan.tomic@factoryww.com>
2021-12-28 11:35:36 +0100

    GrafikoniAPI

--- src/Tabele/PregledIzvestaja/IzvestajiKlijenti.jsx
+++ src/Tabele/PregledIzvestaja/IzvestajiKlijenti.jsx
@@ -1,29 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Row, Col, Typography } from 'antd/lib';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import 'antd/dist/antd.css';
 import styles from './PregledIzvestaja.module.css';
+import { api } from 'api/api';
 
-const data = [
-  {
-    name: 'Kupac1',
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Kupac2',
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Kupac3',
-    pv: 9800,
-    amt: 2290,
-  },
-];
 const { Title } = Typography;
 
 function IzvestajKlijenti() {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    api.get('/reports/kupci/').then(res => {
+      const list = res.data.map(item => ({ ...item, modal: false }));
+      setData(list);
+    });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  const data01 = [
+    {
+      name: 'Kupac1',
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Kupac2',
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Kupac3',
+      pv: 9800,
+      amt: 2290,
+    },
+  ];
   return (
     <>
       <Row>
@@ -35,7 +47,7 @@ function IzvestajKlijenti() {
           <BarChart
             width={900}
             height={400}
-            data={data}
+            data={data01}
             margin={{
               top: 5,
               right: 30,
@@ -61,7 +73,7 @@ function IzvestajKlijenti() {
           <BarChart
             width={900}
             height={400}
-            data={data}
+            data={data01}
             margin={{
               top: 5,
               right: 30,
